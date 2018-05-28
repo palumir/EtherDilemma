8676af7b9b05e60d821f4f217c1e502d89835964
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-05-28 19:59:05 -0400

    Testing chat stuff

--- ethereum/DilemmaUI/pages/dilemma/controllers/ChatController.js
+++ ethereum/DilemmaUI/pages/dilemma/controllers/ChatController.js
@@ -90,12 +90,12 @@ class ChatController {
 				} else if (json.type === 'history') { // entire message history
 					// insert every single message to the chat window
 					for (var i=0; i < json.data.length; i++) {
-						addMessage(json.data[i].author, json.data[i].text,
+						addMessage(json.data[i].author, json.data[i].text, json.data[i].address,
 								   json.data[i].color, new Date(json.data[i].time));
 					}
 				} else if (json.type === 'message') { // it's a single message
 					input.removeAttr('disabled'); // let the user write another message
-					addMessage(json.data.author, json.data.text,
+					addMessage(json.data.author, json.data.text, json.data.address,
 							   json.data.color, new Date(json.data.time));
 				} else {
 					console.log('Hmm..., I\'ve never seen JSON like this: ', json);
@@ -141,11 +141,15 @@ class ChatController {
 			/**
 			 * Add message to the chat window
 			 */
-			function addMessage(author, message, color, dt) {
-				content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
-					 + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
-					 + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
-					 + ': ' + message + '</p>');
+			function addMessage(author, message, address, color, dt) {
+				
+				// Only show messages from ourself or our partner
+				if(address == web3.eth.accounts[0]) {
+					content.prepend('<p><span style="color:' + color + '">' + author + '</span> @ ' +
+						 + (dt.getHours() < 10 ? '0' + dt.getHours() : dt.getHours()) + ':'
+						 + (dt.getMinutes() < 10 ? '0' + dt.getMinutes() : dt.getMinutes())
+						 + ': ' + message + '</p>');
+				}
 			}
 		});
 	}
