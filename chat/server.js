64aa66031dcfc48847051781d5dc09eb737eb036
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-05-28 19:39:23 -0400

    Testing chat stuff

--- chat/server.js
+++ chat/server.js
@@ -76,10 +76,10 @@ wsServer.on('request', function(request) {
     connection.on('message', function(message) {
         if (message.type === 'utf8') { // accept only text
 			
-			console.log('Message: ' + message.utf8data);
+			console.log('Message: ' + message.utf8Data);
 			
 			// Unpack message
-			var unpacked = JSON.parse(message.utf8data);
+			var unpacked = JSON.parse(message.utf8Data);
 			message = unpacked.message;
 			//var userAddress = unpacked.address;
 		
