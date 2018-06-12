3824a264d9a0a3bd9b96d89fbf8ee1ea4c7c6db8
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-06-12 07:56:49 -0400

    Tutorial

---
+++ faq.php
@@ -0,0 +1,165 @@
+<html lang="en">
+
+	<?php include('templates/head/head.php'); ?>
+
+	<body>
+
+	<div class="container-full">
+
+		<?php include('templates/header/header.php'); ?>
+		
+		<?php include('ethereum/DilemmaUI/DilemmaUI.php') ?>
+		<?php include('ethereum/DilemmaUI/pages/dilemma/Dilemma.php') ?>
+		
+		<main id="main" role="main" class="FAQ container-full col-sm-10">
+		<h1>Frequently Asked Questions</h1>
+		<h2>General</h2>
+<div id="general">
+<h3>What is EtherDilemma?</h3>
+
+<p>EtherDilemma is a high stakes, head-to-head game of trust and deception. Players face the seemingly simple dilemma: ally your opponent and split the payout… or betray to win (and risk) it all.</p>
+
+<h3>What makes EtherDilemma different from other games?</h3>
+
+<p>EtherDilemma is one of only a handful of games built on the decentralized Ethereum blockchain (called dApps), and the first to create a live, player-versus-player wagering system.</p>
+
+<h3>What is the prisoner’s dilemma?</h3>
+
+<p>The prisoner’s dilemma is a paradox in which two individuals (in this case, complete strangers) decide between cooperating to receive a lesser, mutually beneficial reward, or opt to betray their opponent to claim the full reward for themselves. The reward is severely reduced for both individuals shall they both betray. Choose wisely.</p>
+
+<h3>What do I need to play EtherDilemma?</h3>
+
+<p>To play EtherDilemma, you’ll need:
+
+A computer (PC or laptop) with a browser capable of supporting MetaMask - we recommend Google Chrome or FireFox. 
+MetaMask, a digital wallet extension that allows you to run dApps (apps on the ethereum blockchain... aka EtherDilemma) securely on your browser.
+Ether on your MetaMask account - learn how to purchase ETH here. </p>
+
+<h3>Who is my opponent? Will I be able to recognize my opponent?</h3> 
+
+<p>Your opponent is a player found via randomized matchmaking. Players can choose a custom ‘username’ in each match (and change it at their discretion), thereby preserving the anonymity of EtherDilemma.</p>
+
+<h3>Does it cost money to play?</h3>
+
+<p>Potentially - every match requires both players to wager 20 finney (what is a finney? Read more below.) on the outcome of the Dilemma. Depending on each players’ choice to ally or betray, the payout ranges between 0 and 36 finney per player per match (see the full Payout breakdown here).</p>
+
+<h3>What is a ‘finney’?</h3>
+
+<p>A finney is one one-thousandths of an ether (ETH), meaning the value of one finney fluctuates with the value of ether. For example, if one ETH is worth $500, a finney is 1/1000 of $500, or 50 cents.</p>
+
+<h3>Can I use USD or another currency instead of ETH?</h3> 
+
+<p>No - the only way to play EtherDilemma is to own ether on a digital wallet. However, you can purchase ETH with USD and other currencies - read more on How to Purchase ETH here (*Link to crypto section).</p>
+
+<h3>Can I play without wagering finney?</h3> 
+
+<p>Nope - EtherDilemma is a high-stakes game of trust and deception. Both players need to have a little skin in the game.</p>
+</div>
+
+
+<h2>Cryptocurrency</h2>
+
+<div id="cryptocurrency">
+<h3>What is Ethereum and ETH?</h3>
+
+<p>Ethereum is an open-source platform capable of running smart contracts - aka decentralized apps (called dApps). The cryptocurrency Ether (ETH) itself powers the network, thereby allowing the applications to run. </p>
+
+<h3>How do I purchase ETH?</h3>
+
+<p>ETH can be purchased directly on MetaMask if you live within the United States. Simply click the MetaMask extension on your browser, make sure the ‘Main Ethereum Network’ is selected, and click ‘Buy’. You’ll then be directed to Coinbase to complete the purchase. 
+
+For international users, you’ll need to purchase ETH directly on a digital exchange and follow their instructions for sending it to your MetaMask account. Your MetaMask account address can be found by clicking the three dots to the right of your account name (name is ‘Account 1’ in the example below). </p>
+
+<h3>Is EtherDilemma an ICO? Is there a coin or currency?</h3> 
+
+<p>No to all! EtherDilemma is simply a game built on the ethereum blockchain. There is no ICO or ‘coin’ associated with EtherDilemma. </p>
+
+<h3>What is Metamask?</h3>
+
+<p>MetaMask is a digital wallet extension that allows you to run dApps (apps on the ethereum blockchain... aka EtherDilemma) securely on your browser. To learn more on MetaMask and how to install, head to the MetaMask site.</p>
+
+<h3>How do I install Metamask?</h3>
+
+<p>MetaMask can be downloaded and used on several browsers (we recommend Google Chrome) by heading to metamask.io. Be sure to write down your seed words (used to reset your password if lost or forgotten) and password so you are not locked out of your account. </p>
+
+<h3>Re-installing - save your seed words</h3>
+
+<p>If you encounter an issue with MetaMask and the extension is no longer working, you can easily uninstall/delete the extension, re-download MetaMask, and setup your account using the same seed words. </p>
+
+<h3>How do I add my ETH to Metamask?</h3>
+
+<p>MetaMask allows you to purchase ETH directly within the extension (screenshot of buy button) if you live within the United States. International users will need to purchase ETH via an external exchange then transfer their ETH to MetaMask (list out steps). </p>
+
+<h3>Are EtherDilemma and MetaMask secure?</h3>
+
+<p> No. </p>
+
+<h3>What is ‘gas’?</h3>
+
+<p>‘Gas’ is essentially the cost to power a single transaction on the Ethereum network. Unlike traditional games and apps that are stored on a private server, all dApps on the blockchain are decentralized and powered by the shared computational power of all miners on the network. For this reason, all interactions with the blockchain require ‘gas’, or a small fee to complete the process.
+
+Read more about gas and the standard transaction costs here. </p>
+</div>
+
+
+<h2>Gameplay</h2>
+
+<div id="gameplay">
+
+<h3>How do I start a match?</h3>
+
+<p>For first time players, we recommend running through the brief tutorial to setup MetaMask and familiarize yourself with the gameplay. The tutorial will guide you through downloading MetaMask (if you haven’t done so already), starting a match, interacting with your opponent, and solving the Dilemma. 
+
+Seasoned players with MetaMask can simply head to EtherDilemma.com* and hit PLAY.</p>
+
+<h3>Am I allowed to lie to my opponent?</h3>
+
+<p>Truth and deception are a part of the dilemma. You can utilize in-game chat system as much or as little as you like to negotiate with your opponent. Do you trust your opponent? Can your opponent trust you?</p>
+
+<h3>What do I win/lose in each Dilemma? </h3>
+
+<p>To start the match, each player must wager 20 finney.
+If both players ally, each player is returned 24 finney (for a net gain of 4 finney each). 
+If one player betrays and the other allies, the betrayer receives 36 finney and the ally receives 0.
+If both players betray, each receive 6 finney (for a net loss of 14 finney each). 
+
+Choose wisely. </p>
+
+<h3>What happens if I miss my turn?</h3>
+
+<p>Each match lasts exactly X blocks and has a countdown window showing the current blockchain block and the ‘decision block’. Once the decision block is reached, MetaMask will automatically pop up for each player to submit their decision. If either player fails to accept the MetaMask transaction in time, the game will be extended to the next block. 
+
+If a player misses 10 consecutive blocks without submitted their turn, their opponent will be able to report them AFK/unresponsive. ***What happens here? AFK player gets 0, winner gets 36? </p>
+
+<h3>What happens if I select the wrong decision?</h3> 
+
+<p>You must have your decision selected by the time the ‘decision block’ is reached. Whichever option you have highlighted will be locked in and cannot be changed once MetaMask pops up. </p>
+
+<h3>What do I do when my opponent goes AFK/unresponsive?</h3>
+
+<p>If either player is unresponsive and fails to submit their decision for 10 consecutive blocks, their opponent will be able to report them AFK/unresponsive. </p>
+</div>
+
+<script>
+function makeAccordion(selector) {
+	var general = $(selector);
+	general.accordion();
+	general.accordion( "option", "collapsible", true );
+	general.accordion("option","active",false);
+}
+
+makeAccordion("#general" );
+makeAccordion("#cryptocurrency");
+makeAccordion("#gameplay" );
+</script>
+
+		</main>
+		
+		<?php include('templates/footer/footer.php'); ?>
+
+	</div>
+
+	</body>
+</html>
+
+
