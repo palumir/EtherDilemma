3a0e471566f44bb6b0055b6e00f5c5860ca8e3d6
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-06-16 11:51:47 -0400

    Styling

--- ethereum/DilemmaUI/pages/dilemma/views/ChallengeView.js
+++ ethereum/DilemmaUI/pages/dilemma/views/ChallengeView.js
@@ -31,15 +31,15 @@ class ChallengeView {
 		
 		// Build middle section
 		var middleSection = "<div id='gameStuff' class='col-sm-7'>";
-		/*(middleSection += "<div id='theGame' class='col-sm-12'><h4 class='col-lg-4 col-sm-12 left'><img src='images/theGame.png' class='left'></h4><div class='left col-lg-8 col-sm-12 gameStuff'><div class='left col-lg-4 col-sm-12'><div class='col-sm-12'><img src='images/head.png'></div><div class='col-sm-12'>Match with a random player</div></div><div class='left col-lg-4 col-sm-12'><div class='col-sm-12'><img src='images/cash.png'></div><div class='col-sm-12'>Negotiate your alliance</div></div><div class='left col-lg-4 col-sm-12'><div class='col-sm-12'><img src='images/scale.png'></div><div class='col-sm-12'>Decide Ally or Betray</div></div></div></div>";
+		middleSection += "<div id='theGame' class='col-sm-12'><h4 class='col-lg-4 col-sm-12 left'><img src='images/theGame.png' class='left'></h4><div class='left col-lg-8 col-sm-12 gameStuff'><div class='left col-lg-4 col-sm-12'><div class='col-sm-12'><img src='images/head.png'></div><div class='col-sm-12'>Match with a random player</div></div><div class='left col-lg-4 col-sm-12'><div class='col-sm-12'><img src='images/cash.png'></div><div class='col-sm-12'>Negotiate your alliance</div></div><div class='left col-lg-4 col-sm-12'><div class='col-sm-12'><img src='images/scale.png'></div><div class='col-sm-12'>Decide Ally or Betray</div></div></div></div>";
 		middleSection += "<div id='theStakes' class='col-sm-12'><h4 class='col-lg-4 col-sm-12 left'><img src='images/theStakes.png' class='left'></h4><div class='left col-lg-8 col-sm-12 stakesText'>Each player must wager <div class='finney' title='" + finneyText + "'>20 finney</div> to start the dilemma</div></div>";
 		middleSection += "<div id='thePayout' class='col-sm-12'><h4 class='col-lg-4 col-sm-12 left'><img src='images/thePayout.png' class='left'></h4><div class='payouts col-lg-8 col-sm-12 left'><div class='col-lg-4 col-sm-12 left'><img src='images/alliance.png' class='left'></div><div class='col-lg-4 col-sm-12 left'><img src='images/betrayal.png' class='left'></div><div class='col-lg-4 col-sm-12 left'><img src='images/duplicity.png' class='left'></div></div></div>";
 		middleSection += "</div>"; // End gameStuff Div
 		
 		displayObj.append(middleSection);
 		displayObj.append("<div class='col-sm-7' id='bottomText'><button id='challengeButton' title='Please install and login to MetaMask to play Ether Dilemma'>PLAY</button><br>Please download <a href='http://metamask.io'>MetaMask</a> to play Ether Dilemma. See the <a href='/faq.php'>FAQ</a> for more information on installing MetaMask.</div>");
-		createFooter(display);*/
-		displayObj.append("<br><div class='center'>Hello friends, the Beta will be live: <br>Saturday, June 16th 2018 from 12pm - 2pm EST<br><br>Thank you for your interest in our beta! <br><br>Please direct any inquiries to EtherDilemma@gmail.com or join us on Discord at <a href='https://discord.gg/sB8rVns'>https://discord.gg/sB8rVns</a></div>");
+		createFooter(display);
+		/*displayObj.append("<br><div class='center'>Hello friends, the Beta will be live: <br>Saturday, June 16th 2018 from 12pm - 2pm EST<br><br>Thank you for your interest in our beta! <br><br>Please direct any inquiries to EtherDilemma@gmail.com or join us on Discord at <a href='https://discord.gg/sB8rVns'>https://discord.gg/sB8rVns</a></div>");*/
 				
 		// Add tooltip javascript
 		$( document ).tooltip({
