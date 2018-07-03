5d834b90668cd299523ea34bc8544ce8f335b4e3
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-07-02 21:10:24 -0400

    New contracts

--- stats.php
+++ stats.php
@@ -1,302 +1,345 @@
 <script>
 
 	// Locally defined constants for the contracts, for now
-	const CODE_CONTRACT_ADDRESS = "0xe5559612eeb9fc8eb8dc4954927fea4a571c4c4d";
+	const CODE_CONTRACT_ADDRESS = "0x55edeb086759767e367706d919a4239f84e1ee6c";
 	const CODE_CONTRACT_ABI = [
-		{
-			"constant": false,
-			"inputs": [],
-			"name": "cancelChallenge",
-			"outputs": [],
-			"payable": false,
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"constant": false,
-			"inputs": [],
-			"name": "hostChallenge",
-			"outputs": [],
-			"payable": true,
-			"stateMutability": "payable",
-			"type": "function"
-		},
-		{
-			"constant": false,
-			"inputs": [],
-			"name": "init",
-			"outputs": [],
-			"payable": false,
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"constant": false,
-			"inputs": [
-				{
-					"name": "_betray",
-					"type": "bool"
-				}
-			],
-			"name": "makeMove",
-			"outputs": [],
-			"payable": false,
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"anonymous": false,
-			"inputs": [
-				{
-					"indexed": false,
-					"name": "_who",
-					"type": "address"
-				}
-			],
-			"name": "missedTurn",
-			"type": "event"
-		},
-		{
-			"anonymous": false,
-			"inputs": [
-				{
-					"indexed": false,
-					"name": "_who",
-					"type": "address"
-				}
-			],
-			"name": "dilemmaStarted",
-			"type": "event"
-		},
-		{
-			"anonymous": false,
-			"inputs": [
-				{
-					"indexed": false,
-					"name": "_who",
-					"type": "address"
-				}
-			],
-			"name": "challengeCanceled",
-			"type": "event"
-		},
-		{
-			"anonymous": false,
-			"inputs": [
-				{
-					"indexed": false,
-					"name": "_who",
-					"type": "address"
-				}
-			],
-			"name": "challengeHosted",
-			"type": "event"
-		},
-		{
-			"anonymous": false,
-			"inputs": [
-				{
-					"indexed": false,
-					"name": "_who",
-					"type": "address"
-				},
-				{
-					"indexed": false,
-					"name": "_payout",
-					"type": "uint256"
-				},
-				{
-					"indexed": false,
-					"name": "_whoBetray",
-					"type": "bool"
-				},
-				{
-					"indexed": false,
-					"name": "_partnerBetray",
-					"type": "bool"
-				}
-			],
-			"name": "dilemmaFinished",
-			"type": "event"
-		},
-		{
-			"constant": false,
-			"inputs": [],
-			"name": "reportPartnerAFK",
-			"outputs": [],
-			"payable": false,
-			"stateMutability": "nonpayable",
-			"type": "function"
-		},
-		{
-			"inputs": [
-				{
-					"name": "_storageAddress",
-					"type": "address"
-				}
-			],
-			"payable": false,
-			"stateMutability": "nonpayable",
-			"type": "constructor"
-		},
-		{
-			"constant": true,
-			"inputs": [
-				{
-					"name": "",
-					"type": "uint256"
-				}
-			],
-			"name": "challengeList",
-			"outputs": [
-				{
-					"name": "",
-					"type": "address"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": true,
-			"inputs": [
-				{
-					"name": "",
-					"type": "address"
-				}
-			],
-			"name": "challenges",
-			"outputs": [
-				{
-					"name": "active",
-					"type": "bool"
-				},
-				{
-					"name": "number",
-					"type": "uint256"
-				},
-				{
-					"name": "randomNumber",
-					"type": "uint256"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": true,
-			"inputs": [
-				{
-					"name": "",
-					"type": "address"
-				}
-			],
-			"name": "dilemmas",
-			"outputs": [
-				{
-					"name": "partner",
-					"type": "address"
-				},
-				{
-					"name": "active",
-					"type": "bool"
-				},
-				{
-					"name": "lastTurnBlock",
-					"type": "uint256"
-				},
-				{
-					"name": "betray",
-					"type": "bool"
-				},
-				{
-					"name": "turnsMissed",
-					"type": "uint256"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": true,
-			"inputs": [],
-			"name": "getLastTurnBlock",
-			"outputs": [
-				{
-					"name": "",
-					"type": "uint256"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": true,
-			"inputs": [],
-			"name": "getPartnerAddress",
-			"outputs": [
-				{
-					"name": "",
-					"type": "address"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": true,
-			"inputs": [],
-			"name": "getPartnerLastTurnBlock",
-			"outputs": [
-				{
-					"name": "",
-					"type": "uint256"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": true,
-			"inputs": [],
-			"name": "isChallengeActive",
-			"outputs": [
-				{
-					"name": "",
-					"type": "bool"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		},
-		{
-			"constant": true,
-			"inputs": [],
-			"name": "isDilemmaActive",
-			"outputs": [
-				{
-					"name": "",
-					"type": "bool"
-				}
-			],
-			"payable": false,
-			"stateMutability": "view",
-			"type": "function"
-		}
-	];
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_who",
+				"type": "address"
+			}
+		],
+		"name": "missedTurn",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_who",
+				"type": "address"
+			}
+		],
+		"name": "challengeHosted",
+		"type": "event"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "cancelChallenge",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_who",
+				"type": "address"
+			}
+		],
+		"name": "challengeCanceled",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_who",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "_payout",
+				"type": "uint256"
+			},
+			{
+				"indexed": false,
+				"name": "_whoBetray",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"name": "_partnerBetray",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"name": "_youAreAFK",
+				"type": "bool"
+			},
+			{
+				"indexed": false,
+				"name": "_theyAreAFK",
+				"type": "bool"
+			}
+		],
+		"name": "dilemmaFinished",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "_who",
+				"type": "address"
+			}
+		],
+		"name": "dilemmaStarted",
+		"type": "event"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "hostChallenge",
+		"outputs": [],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_betray",
+				"type": "bool"
+			}
+		],
+		"name": "makeMove",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "reportPartnerAFK",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_storageAddress",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"name": "challengeList",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "challenges",
+		"outputs": [
+			{
+				"name": "active",
+				"type": "bool"
+			},
+			{
+				"name": "number",
+				"type": "uint256"
+			},
+			{
+				"name": "randomNumber",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"name": "dilemmas",
+		"outputs": [
+			{
+				"name": "partner",
+				"type": "address"
+			},
+			{
+				"name": "active",
+				"type": "bool"
+			},
+			{
+				"name": "lastTurnBlock",
+				"type": "uint256"
+			},
+			{
+				"name": "betray",
+				"type": "bool"
+			},
+			{
+				"name": "turnsMissed",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getBlocksToDecide",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getBlocksUntilAFK",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getLastTurnBlock",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getMissedTurnsUntilCheating",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "pure",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getPartnerAddress",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "getPartnerLastTurnBlock",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "isChallengeActive",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "isDilemmaActive",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
 
 	var codeContract = web3.eth.contract(CODE_CONTRACT_ABI);
 	codeContract = codeContract.at(CODE_CONTRACT_ADDRESS);
 	
 	// watch for an event with {some: 'args'}
-	var events = codeContract.allEvents({fromBlock: 3449295, toBlock: 3450032});
+	var events = codeContract.allEvents({fromBlock: 0, toBlock: "latest"});
 	events.watch(function(error, result){
 	});
 
