48c2ca0ccd0b828565fac26cfa048ccec072d8fd
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-07-08 02:32:50 -0400

    Calling functionality added, backdoor added

--- ethereum/DilemmaUI/pages/dilemma/views/MoveView.js
+++ ethereum/DilemmaUI/pages/dilemma/views/MoveView.js
@@ -22,7 +22,7 @@ class MoveView {
 			
 			// Only create it if it doesn't exist yet
 			if($('#moveSelector')[0]==undefined) {
-				$("#" + display).append("<div id='moveSelector' class='col-sm-12' ><div class='selectable ally col-sm-6' value=0></div><div class='selectable betray col-sm-6' value=1></div><div class='selectable call col-sm-6' value=2>Call</div></div>");
+				$("#" + display).append("<div id='moveSelector' class='col-sm-12' ><div class='selectable ally col-sm-4' value=0></div><div class='selectable betray col-sm-4' value=1></div><div class='selectable call col-sm-4' value=2>Call</div></div>");
 				$("#" + display).append("<div id='updateBlock' class='col-sm-12' ><div class='title'>Choose Wisely</div><div class='text'><div class='allyUnderline'>ALLY</div>, <div class='betrayUnderline'>BETRAY</div>, or <div class='callUnderline'>CALL</div></div></div>");
 			}
 	}
