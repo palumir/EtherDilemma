52db996698f04cead1de655994f9d31f315cf6f1
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-07-10 02:21:36 -0400

    Chat styling

--- ethereum/DilemmaUI/pages/dilemma/views/ChatView.js
+++ ethereum/DilemmaUI/pages/dilemma/views/ChatView.js
@@ -17,7 +17,7 @@ class ChatView {
 	// Append HTML to displayDiv
 	appendHTML() {
 		
-		this.controller.displayDiv.append('<div id="chatHolder" class="col-sm-6"><h3 class="col-sm-12"><img src="images/negotiation.png"></h3><ul id="messages"></ul><form action=""><input id="m" autocomplete="off" /><button>Send</button></form></div>');
+		this.controller.displayDiv.append('<div id="chatHolder" class="col-sm-6"><h3 class="col-sm-12"><img src="images/negotiation.png"></h3><ul id="messages"></ul><form action=""><input id="m" class="col-sm-9" autocomplete="off" /><button class="col-sm-3">Send</button></form></div>');
 	}
 	
 }
