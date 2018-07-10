cf6a1baa0e7701c0c773d740fee8672bff3db9d3
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-07-09 22:45:54 -0400

    Making socket IO behave

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var port = process.env.PORT || 1337;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -8,9 +9,10 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-	io.emit('chat message', msg);
-	console.log("msg: " + msg);
+    io.emit('chat message', msg);
   });
 });
 
-http.listen(1337, "127.0.0.1");
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
