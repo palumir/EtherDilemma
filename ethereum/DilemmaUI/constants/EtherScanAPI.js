0c38a66049a94c8dcb8c293b997eb0f06f09fc06
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-07-26 00:43:30 -0400

    Front images

--- ethereum/DilemmaUI/constants/EtherScanAPI.js
+++ ethereum/DilemmaUI/constants/EtherScanAPI.js
@@ -2,3 +2,5 @@ var ETHERSCAN_API_KEY = "BPTZH8Z2RVE1ZRSHZEBET72NNIREUIFHJ6";
 
 var ETHERSCAN_BASE_URL = "";
 
+// Both constants are pulled AFTER MetaMask injects web3 from web3 object in DilemmaUI.js file
+
