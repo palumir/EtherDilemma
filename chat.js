d1afdf497f0262486b41339eeb221272bf37a52f
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-09-04 06:29:47 -0400

    NodeJS parsing json better

--- chat.js
+++ chat.js
@@ -32,7 +32,8 @@ function intervalFunc() {
 
 	  // The whole response has been received. Print out the result.
 	  resp.on('end', () => {
-		console.log(JSON.parse(data).result);
+		blockNumber = parseInt(JSON.parse(data).result.result,16);
+		console.log(blockNumber);
 	  });
 
 	}).on("error", (err) => {
