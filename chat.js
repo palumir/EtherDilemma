0eb84a05035b6a36727c3430611580edde08cc98
Andrew Jenkins <andrew123jenkins@gmail.com>
2018-09-04 06:04:42 -0400

    New nodejs server

--- chat.js
+++ chat.js
@@ -2,6 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 1337;
+const https = require('https');
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
@@ -14,7 +15,7 @@ var $ = require('jquery');
 // Get the block number
 function intervalFunc() {
 	/* api.etherscan.io */
-	$.get("https://ropsten.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=BPTZH8Z2RVE1ZRSHZEBET72NNIREUIFHJ6", function(data, status) {
+	https.get("https://ropsten.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=BPTZH8Z2RVE1ZRSHZEBET72NNIREUIFHJ6", function(data, status) {
 		
 		// Set current block number, so we may use it in another callback
 		blockNumber = parseInt(data.result,16);
